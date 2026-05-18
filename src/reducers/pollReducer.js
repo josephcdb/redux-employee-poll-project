146bbf9dac8e0f10aafd4d1fc565455902fa9df6
Joseph <chua.debao@gmail.com>
2026-05-18 12:19:22 +0200

    Update code on reducer part

--- src/reducers/pollReducer.js
+++ src/reducers/pollReducer.js
@@ -1,11 +1,25 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { _getQuestions } from '../data/_DATA'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { _getQuestions, _saveQuestionAnswer } from '../data/_DATA';
 
 export const fetchQuestions = createAsyncThunk(
-  'polls/fetchQuestions',
-  async () => {
-    const questions = await _getQuestions()
-    return questions
+  'polls/fetchQuestions', async () => {
+    return await _getQuestions()
+  }
+)
+
+export const saveQuestionAnswer = createAsyncThunk(
+  'polls/saveQuestionAnswer', async ({ authedUser, qid, answer }) => {
+    await _saveQuestionAnswer({
+      authedUser,
+      qid,
+      answer,
+    })
+
+    return {
+      authedUser,
+      qid,
+      answer,
+    }
   }
 )
 
@@ -13,10 +27,29 @@ const pollReducer = createSlice({
   name: 'polls',
   initialState: {},
   reducers: {},
+
   extraReducers: (builder) => {
-    builder.addCase(fetchQuestions.fulfilled, (state, action) => {
-      return action.payload
-    })
+    builder.addCase(
+      fetchQuestions.fulfilled,
+      (_, action) => {
+        return action.payload
+      }
+    )
+
+    builder.addCase(
+      saveQuestionAnswer.fulfilled,
+      (state, action) => {
+        const {
+          authedUser,
+          qid,
+          answer,
+        } = action.payload
+
+        state[qid][answer].votes.push(
+          authedUser
+        )
+      }
+    )
   },
 })
 
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,10 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { _getUsers } from '../data/_DATA'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { _getUsers } from '../data/_DATA';
+import { saveQuestionAnswer } from './pollReducer';
 
 export const fetchUsers = createAsyncThunk(
-  'users/fetchUsers',
-  async () => {
-    const users = await _getUsers()
-    return users
+  'users/fetchUsers', async () => {
+    return await _getUsers()
   }
 )
 
@@ -13,10 +12,28 @@ const userReducer = createSlice({
   name: 'users',
   initialState: {},
   reducers: {},
+
   extraReducers: (builder) => {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload
-    })
+    builder.addCase(
+      fetchUsers.fulfilled,
+      (_, action) => {
+        return action.payload
+      }
+    )
+
+    builder.addCase(
+      saveQuestionAnswer.fulfilled,
+      (state, action) => {
+        const {
+          authedUser,
+          qid,
+          answer,
+        } = action.payload
+
+        state[authedUser].answers[qid] =
+          answer
+      }
+    )
   },
 })
 
